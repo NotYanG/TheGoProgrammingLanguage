0af080dada76e8d6677aec0386fdc1613a595cb2
NotYanG <353218571@qq.com>
2022-05-16 20:13:32 +0800

    1.2: 输出命令行参数

---
+++ ch1/ch1_echo.go
@@ -0,0 +1,36 @@
+// echo1输出其命令行参数
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func echo1() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func echo2() {
+	/* 另一种形式的for循环 */
+	var s, sep string
+	for _, arg := range os.Args {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func echo3() {
+	/* 使用strings包中的Join函数 */
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+func main() {
+
+}
